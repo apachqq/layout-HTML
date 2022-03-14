71517d57ed2023aecfd68b1878e6f8562a700d26
apachqq <work.tsyganov@mail.ru>
2022-03-15 01:14:45 +0300

    'addEvent'

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 let orders = document.querySelector('.orders')
 let direction = document.querySelector('.direction')
 let howWork = document.querySelector('.how-work')
+
+window.addEventListener('scroll', function() {
+
+})
